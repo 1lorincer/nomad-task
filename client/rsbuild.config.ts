2c4d675195548e8791fa22cdd31087d2fd6e7843
asianParadissseee <asian.paradissseee@gmail.com>
2025-08-09 21:45:44 +0500

    added tailwind

--- client/rsbuild.config.ts
+++ client/rsbuild.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginVue } from '@rsbuild/plugin-vue';
+import {defineConfig} from '@rsbuild/core';
+import {pluginVue} from '@rsbuild/plugin-vue';
 
 export default defineConfig({
-  plugins: [pluginVue()],
+    plugins: [pluginVue()],
+    tools: {
+        postcss: {
+            postcssOptions: {
+                plugins: [
+                    require('@tailwindcss/postcss')
+                ]
+            }
+        }
+    }
 });
